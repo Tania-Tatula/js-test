db74025eb18724d8af83ed594750ea473c0185ed
Tania Tatula <tgrogol@gmail.com>
2021-06-01 19:14:22 +0300

    стилізує код

--- src/js/js-1.js
+++ src/js/js-1.js
@@ -1,21 +1,18 @@
 const removeListenerBtn = document.querySelector('.subtract');
 const addListenerBtn = document.querySelector('.add');
-const  counterValue = document.querySelector("#value");
-
+const counterValue = document.querySelector('#value');
 
 // додає 1 до числа в інпут
 const increment = () => {
-    
-    return counterValue.value = Number(counterValue.value) + 1;
+  return (counterValue.value = Number(counterValue.value) + 1);
 };
 
 // віднімає 1 до числа в інпут
 
 const decrement = () => {
-    return counterValue.value = Number(counterValue.value) - 1;
+  return (counterValue.value = Number(counterValue.value) - 1);
 };
-
 
 // запускаємо функції додавання та відніманн по кліку
 addListenerBtn.addEventListener('click', increment);
-removeListenerBtn.addEventListener('click', decrement)
+removeListenerBtn.addEventListener('click', decrement);
--- src/js/js-3.js
+++ src/js/js-3.js
@@ -1,29 +1,58 @@
 const hero = document.querySelector('.hero');
-const names = ['Devileye', 'Oups', 'DazYazab', 'Mr3vil', 'Sunisup', 'Nanilui', 'Prezakas', 'WukOng', 'Lio', 'AndroidNumero16', 'Uberzoe', 'Nocturne', 'Wio', 'Curaj', 'SirMime', 'Kurama', 'Kukukoko', 'MaxPad', 'Sibbi', 'Zolker'];
-const heroNames =[];
+const names = [
+  'Devileye',
+  'Oups',
+  'DazYazab',
+  'Mr3vil',
+  'Sunisup',
+  'Nanilui',
+  'Prezakas',
+  'WukOng',
+  'Lio',
+  'AndroidNumero16',
+  'Uberzoe',
+  'Nocturne',
+  'Wio',
+  'Curaj',
+  'SirMime',
+  'Kurama',
+  'Kukukoko',
+  'MaxPad',
+  'Sibbi',
+  'Zolker',
+];
+const heroNames = [];
 
 // створюємо генератор випадковх чисел
 function fontSize(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  };
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
 
 // створюємо генератор випадкової розміру літер
-  function textTransform() {
-    let transform = '';
-      let number = fontSize(1, 2);
-      if(number == 1){
-        transform = 'capitalize';
-      }
-      else{
-        transform = 'uppercase';
-      }
-      return transform;
+function textTransform() {
+  let transform = '';
+  let number = fontSize(1, 2);
+  if (number == 1) {
+    transform = 'capitalize';
+  } else {
+    transform = 'uppercase';
   }
+  return transform;
+}
 
 //   створюємо розмітку кожного імені героя разом зі стилями
-  names.map(name => heroNames.push(
-      `<p style="font-size: ${fontSize(10, 14)}px; text-transform:${textTransform()}; margin-top: ${fontSize(5, 12)}px;">${name}</p>`));
+names.map(name =>
+  heroNames.push(
+    `<p style="font-size: ${fontSize(
+      10,
+      14,
+    )}px; text-transform:${textTransform()}; margin-top: ${fontSize(
+      5,
+      12,
+    )}px;">${name}</p>`,
+  ),
+);
 
 // рендеримо розмітку
-  hero.innerHTML = heroNames.join(' ');
+hero.innerHTML = heroNames.join(' ');
